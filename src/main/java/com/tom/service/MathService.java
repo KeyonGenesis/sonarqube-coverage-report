66cf2483214ebb8e797403c2970e284ab8a37bb7
Keyon Genesis Kanan <keyon_genesis_kanan@hdb.gov.sg>
2021-11-29 13:29:29 +0800

    new changes for bad code

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -1,3 +1,4 @@
+
 package com.tom.service;
 
 public class MathService {
@@ -7,6 +8,11 @@ public class MathService {
     }
 
     public int subtract(int firstNumber, int secondNumber) {
+        #return firstNumber - secondNumber;
+	    
+   	Integer myInt = null;
+        Integer yourInt = myInt * 2;
         return firstNumber - secondNumber;
-    }
+
+	}
 }
